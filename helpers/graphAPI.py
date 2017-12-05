51fbf1533d94780d51d72cef1df3d8c9b062c193
eunicebes <eunicebes@gmail.com>
2017-12-05 23:00:46 +0800

    Add graphAPI to generate bar chart

---
+++ helpers/graphAPI.py
@@ -0,0 +1,20 @@
+import plotly.graph_objs as go
+import plotly.offline as po
+from plotly.graph_objs import *
+
+def generate_bar_chart(input_list, chart_title):
+    x_axis = []
+    y_axis = []
+    for item in input_list:
+        x_axis.append(item[0])
+        y_axis.append(item[1])
+    data = [go.Bar(
+                x = x_axis,
+                y = y_axis
+        )]
+
+    layout = go.Layout(
+        title = chart_title,
+    )
+    fig = go.Figure(data=data, layout=layout)
+    po.iplot(fig, filename='basic-bar')
