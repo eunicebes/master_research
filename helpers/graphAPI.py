d9bdcfa28b52e747ef45c66a849c6e79d0955963
eunicebes <eunicebes@gmail.com>
2017-12-25 23:15:30 +0800

    Trying tfidf for tweets

--- helpers/graphAPI.py
+++ helpers/graphAPI.py
@@ -26,3 +26,16 @@ def generate_histogram(input_list, chart_title):
     )
     fig = go.Figure(data=data, layout=layout)
     po.iplot(fig, filename='basic histogram')
+    
+def generate_stacked_histogram(input_list_1, input_list_2, chart_title):
+    trace0 = go.Histogram(
+        x=input_list_1
+    )
+    trace1 = go.Histogram(
+        x=input_list_2
+    )
+    data = [trace0, trace1]
+    layout = go.Layout(barmode='stack', title=chart_title)
+    fig = go.Figure(data=data, layout=layout)
+
+    po.iplot(fig, filename='stacked histogram')
